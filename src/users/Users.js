8c721cce4cea882f90e1926dd2cc04b1eba26146
Stephanie <stephieplusthree@gmail.com>
2020-04-28 19:15:25 -0700

    comp

--- src/users/Users.js
+++ src/users/Users.js
@@ -15,7 +15,27 @@ class Users extends Component {
     
     // function to activate when user clicks on button
     makeMeYounger = () => {
-        console.log('clicked');
+
+        // create a map function that will create a tempuser for each user it will subtract age by 10
+        const newState = this.state.users.map((user)=>{
+            const tempUser = user;
+            tempUser.age -=10;
+            return tempUser;
+        });
+        // you will get a new state and pass that state
+        this.setState({
+            newState
+        });
+
+        // setState by passing an object
+        // this.setState({
+        //     users: [
+        //         {name: "John", age: 10},
+        //         {name: "Jill", age: 20},
+        //         {name: "Peter", age: 30},
+        //     ]
+        // })
+        // console.log('clicked');
     }
 
     render() {
@@ -27,10 +47,15 @@ class Users extends Component {
                 <br/>
                 {/* display text as h1 */}
                 <h1>{this.state.title}</h1>
+                {
+                    this.state.users.map((user)=>{
+                        return <User age={user.age}>{user.name}</User>
+                    })
+                }
                 {/* get rid of the hard coded stuff. Remove the age and use curly brackets and inside this state is available. You can access it by using this dot state */}
-                <User age={this.state.users[0].age}>{this.state.users[0].name}</User>
+                {/* <User age={this.state.users[0].age}>{this.state.users[0].name}</User>
                 <User age={this.state.users[1].age}>{this.state.users[1].name}</User>
-                <User age={this.state.users[2].age}>{this.state.users[2].name}</User>
+                <User age={this.state.users[2].age}>{this.state.users[2].name}</User> */}
                 {/* create multiple user components */}
                 {/* <User age="20">John</User>
                 <User age="30">Jill</User>
